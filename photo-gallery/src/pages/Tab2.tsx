9b552e6aa5091e57149ab337f5f8002c4465ee93
Zorokyn <scraggy1998@gmail.com>
2022-04-08 13:23:14 +0100

    U delete function

--- photo-gallery/src/pages/Tab2.tsx
+++ photo-gallery/src/pages/Tab2.tsx
@@ -14,16 +14,18 @@ import {
   IonImg,
   IonActionSheet,
 } from '@ionic/react';
+import React, { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 //HOOKS
-import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 
 
 const Tab2: React.FC = () => {
 
-  const { photos, takePhoto } = usePhotoGallery();
+  const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
 
   return (
     <IonPage>
@@ -49,12 +51,34 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size="6" key={index}>
-                <IonImg src={photo.webviewPath} />
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath} />
               </IonCol>
             ))}
           </IonRow>
         </IonGrid>
       </IonContent>
+      <IonActionSheet
+        isOpen={!!photoToDelete}
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            icon: trash,
+            handler: () => {
+              if (photoToDelete) {
+                deletePhoto(photoToDelete);
+                setPhotoToDelete(undefined);
+              }
+            },
+          },
+          {
+            text: 'Cancel',
+            icon: close,
+            role: 'cancel',
+          },
+        ]}
+        onDidDismiss={() => setPhotoToDelete(undefined)}
+      />
       </IonContent>
     </IonPage>
   );
